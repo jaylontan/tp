a81ed4932fdb60351734b6224137c6153f6336b6
Ben926 <benjamin.koh.ts@gmail.com>
2025-03-25 23:03:55 +0800

    Fix styling

--- src/main/java/seedu/address/logic/parser/EditBookingCommandParser.java
+++ src/main/java/seedu/address/logic/parser/EditBookingCommandParser.java
@@ -2,7 +2,12 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_BOOKING_ID;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PAX;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
+
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
