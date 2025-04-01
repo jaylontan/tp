f003ba3c3e8b4ff1c26852805a033150550d7062
Ben926 <benjamin.koh.ts@gmail.com>
2025-04-01 22:45:26 +0800

    Edit clear command word

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -10,8 +10,8 @@ import seedu.address.model.Model;
  */
 public class ClearCommand extends Command {
 
-    public static final String COMMAND_WORD = "clear";
-    public static final String MESSAGE_SUCCESS = "Address book has been cleared!";
+    public static final String COMMAND_WORD = "resetall";
+    public static final String MESSAGE_SUCCESS = "Address book and all bookings have been cleared!";
 
 
     @Override
