a3052247c0164d3af72ee8cc75bba4ac907315d6
jaylontan <156635750+jaylontan@users.noreply.github.com>
2025-04-03 09:37:38 +0800

    Update AddCommand.java
    - Update example command
    - Remove phone number validation logic in AddCommandParser.java

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -35,7 +35,7 @@ public class AddCommand extends Command {
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_ISMEMBER + "True "
-            + PREFIX_TAG + "Allergic to nuts ";
+            + PREFIX_TAG + "AllergicToNuts ";
 
     public static final String MESSAGE_SUCCESS = "New person added: %1$s";
     public static final String MESSAGE_DUPLICATE_PERSON = "This phone number already exists in the address book";
